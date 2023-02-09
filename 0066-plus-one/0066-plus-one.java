1a1578b8b01b3f5a3926d5cf94d0d1453acc5997
HarshKumarDev <122696927+HarshKumarDev@users.noreply.github.com>
2023-02-09 23:35:50 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (63.69%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -15,19 +15,3 @@ class Solution {
         return ans;
     }
 }
-    //     for (int i=0;i<digits.length;i++)
-    //     {
-    //         int d=digits[i];
-    //         int n=n*10+d;
-    //     }
-    //     int n=n+1;
-    //     int l=digits.length;
-    //     ArrayList<Integer>List=new ArrayList<>(10);
-    //     for (int j=digits.length-1;j>=0;j--)
-    //     {
-    //         int d=n%10;
-    //         List.add(d);
-    //         n=n/10;
-    //     }
-    //     return List;
-    // }
