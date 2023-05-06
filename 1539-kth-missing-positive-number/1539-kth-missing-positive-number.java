1f74219872e69a11320c34c63d9272fa0f60cf0a
HarshKumarDev <122696927+HarshKumarDev@users.noreply.github.com>
2023-05-06 13:40:50 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (31.64%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,22 +1,26 @@
 class Solution {
-    public int findKthPositive(int[] arr, int k) {
-        int n = arr.length;
-        int missingCount = arr[0] - 1; // Count of missing numbers before the first element
-        if (missingCount >= k) {
-            return k; // The kth missing number is less than the first element
+    public int findKthPositive(int[] arr, int k) 
+    {
+        int num=1;
+        int n=arr.length;
+        int i=0;
+      while (i<n && k>0)
+      {
+          if (arr[i] == num) //not missing (so checking with next element by increasing i)
+              i++; 
+          else
+          if (arr[i] != num)
+          {
+              k--;
+          }
+          num++;
+      }
+        while (k>=0)
+        {
+            k--;
+            num++;
         }
-        
-        for (int i = 1; i < n; i++) {
-            int diff = arr[i] - arr[i - 1] - 1; // Count of missing numbers between the current and previous element
-            if (missingCount + diff >= k) {
-                // The kth missing number is between the previous and current element
-                return arr[i - 1] + (k - missingCount);
-            }
-            
-            missingCount += diff;
-        }
-        
-        // The kth missing number is greater than the last element
-        return arr[n - 1] + (k - missingCount);
+        return num-2;
     }
 }
+              
