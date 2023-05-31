1b18068942a891f660ad07211ee830ffd9cd12e8
HarshKumarDev <122696927+HarshKumarDev@users.noreply.github.com>
2023-06-01 02:45:25 +0530

    Time: 2 ms (93.91%), Space: 40.1 MB (99.50%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) 
+    {
+        Arrays.sort(arr);
+     ArrayList<Integer> hs =new ArrayList<>(); 
+        int count=1;
+     for (int i=0;i<arr.length-1;i++)
+     { 
+         int n=arr[i];
+         if (arr[i] == arr[i+1])
+         {
+             count++;
+         }
+         else
+         {
+             if (hs.contains(count))
+                 return false;
+             hs.add(count);
+             count=1;
+         }
+     }
+       // hs.add(count);  // last element
+                if(hs.contains(count)) 
+                    return false;
+        else
+            return true;
+     }
+}
+         
+             
+             
